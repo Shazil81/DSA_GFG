a5bc714f3ab35351c3766d2eaf8f67f1d5e8aa4f
Shazil81 <ahmadshahab890@gmail.com>
2026-06-27 20:45:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.py
@@ -0,0 +1,40 @@
+class Solution:
+    def lowerBound(self, nums, target):
+        lb = -1
+        n = len(nums)
+        low = 0
+        high = n-1
+        while low<=high:
+            mid = (low+high)//2
+            if nums[mid]>=target:
+                lb = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return lb
+    # Upper Bound code for last occurrence
+    def upperBound(self, nums, target):
+        ub = -1
+        n = len(nums)
+        low = 0
+        high = n-1
+        while low<=high:
+            mid = (low+high)//2
+            if nums[mid]>target:
+                ub = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ub
+    def find(self, nums, target):
+        lb = self.lowerBound(nums, target) # calling Lower Bound
+        if lb == -1 or nums[lb]!= target: # checking if the target is not in the nums
+            return [-1, -1]
+        ub = self.upperBound(nums, target) # calling Upper bound
+
+        if ub == -1:
+            return [lb, len(nums) - 1]
+        
+        return [lb, ub-1]
+        
+        
