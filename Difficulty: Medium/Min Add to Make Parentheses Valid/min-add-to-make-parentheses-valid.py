b002de510564f7c9f2fcf568de50062be898a648
Shazil81 <ahmadshahab890@gmail.com>
2026-07-01 21:25:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Add to Make Parentheses Valid/min-add-to-make-parentheses-valid.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minParentheses(self, s):
+        # Without using Stack
+
+        left_bracket, right_bracket = 0, 0
+
+        for ch in s:
+            if ch == '(':
+                left_bracket += 1
+            elif ch == ')' and left_bracket > 0:
+                left_bracket -= 1
+            else:
+                right_bracket += 1
+        
+        return left_bracket + right_bracket
+        
+        
