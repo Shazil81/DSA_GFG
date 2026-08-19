b3efe1a627009b59d74885e2a28989c45540499a
Shazil81 <ahmadshahab890@gmail.com>
2026-08-19 17:24:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, index, res, nums):
+    # base case
+        if index >= len(nums):
+            res.append(nums.copy())
+            return
+    
+        vis = set()
+    
+        for i in range(index, len(nums)):
+            if nums[i] in vis:
+                continue
+    
+            vis.add(nums[i])
+    
+            # Swap: Element ko correct position pe lao
+            nums[index], nums[i] = nums[i], nums[index]
+            # agle index k liye
+            self.solve(index+1, res, nums)
+            # Backtrack kro
+            nums[index], nums[i] = nums[i], nums[index]
+            
+    def uniquePerms(self, arr):
+        res = []
+        self.solve(0, res, arr)
+        res.sort()
+        return res
+        
+        
